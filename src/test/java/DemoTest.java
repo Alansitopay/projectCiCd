088940ed592136d506514195e89eb64d3b42b020
Alansitopay <derpidark@outlook.com>
2025-07-21 21:23:14 -0300

    segundo commit

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,2 +1,10 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class DemoTest {
+    @Test
+    public void testSuma(){
+        Calculadora calc = new Calculadora();
+        Assert.assertEquals(calc.sumar(2,3),5);
+    }
 }
